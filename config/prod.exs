86adaf57332025e86dc0184202f08a4106cb7b06
Michael Collier <m@omginbd.xyz>
2021-01-31 21:22:50 -0700

    fix config

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,8 @@
 import Config
 config :logger, level: :info
 
+config :golfbot, GolfbotWeb.Endpoint, force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
 config :ueberauth, Ueberauth.Strategy.Google.OAuth,
   client_id: System.fetch_env!("GOOGLE_OAUTH_CLIENT_ID"),
   client_secret: System.fetch_env!("GOOGLE_OAUTH_CLIENT_SECRET")
