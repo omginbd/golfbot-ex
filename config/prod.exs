40607feaccb5c2b359d171e104cc2e8c1400b920
Michael Collier <m@omginbd.xyz>
2021-01-31 20:50:54 -0700

    add prod gooogle oauth params

--- config/prod.exs
+++ config/prod.exs
@@ -1,2 +1,6 @@
 import Config
 config :logger, level: :info
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.fetch_env!("GOOGLE_OAUTH_CLIENT_ID"),
+  client_secret: System.fetch_env!("GOOGLE_OAUTH_CLIENT_SECRET")
